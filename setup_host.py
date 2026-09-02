89bb0a831c94875d8314b0a905c559bd88b2a1e8
Abhishek Chand <abhishekchand@gmail.com>
2026-09-02 15:47:00 +0530

    edited

--- setup_host.py
+++ setup_host.py
@@ -19,7 +19,7 @@ else:
 
 
 # 2. Setup Nginx config for it-defined.com (and remove default site)
-run("sudo mv ./it-defined.com.conf /etc/nginx/sites-available/it-defined.com")
+run("sudo mv ./itdefined-config.com.conf /etc/nginx/sites-available/it-defined.com")
 run("sudo ln -sf /etc/nginx/sites-available/it-defined.com /etc/nginx/sites-enabled/it-defined.com")
 run("sudo rm -f /etc/nginx/sites-enabled/default")
 
